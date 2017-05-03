16eb03c6420d377ae44c246fe2a1d760c5f65ff2
Bruno Annunciato <brunobannunciato@gmail.com>
2017-05-03 18:01:02 -0300

    Implementacao do launchpad Implementacao do launchpad + commit dos audios + css + js

---
+++ src/script/launchpad.js
@@ -0,0 +1,24 @@
+window.addEventListener('keydown', function(e){
+  var audio = document.querySelector(`.sound [data-key="${e.key}"]`)
+  var key = document.querySelector(`.key[data-key="${e.key}"`)
+   if(!audio) return;
+    audio.currentTime = 0;
+
+    audio.play();
+    key.classList.add('playing');
+
+
+    setTimeout(function(){
+
+    });
+    function endTransition(e){
+      if(e.propertyName !== 'transform') return;
+      this.classList.remove('playing')
+
+    }
+    /**
+     * @type {NodeList.<HTMLElement>}
+     */
+    var keys = document.querySelectorAll('.key');
+    Array.from(keys).forEach(key => key.addEventListener('transitionend', endTransition));
+});
